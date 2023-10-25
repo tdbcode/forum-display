f41fb8968dc84f613f3f6a268ad1063a0443a411
Thomas Brady <thomasbrady87@gmail.com>
2023-10-25 15:50:35 -0500

    AWS dynamo P1

--- src/App.js
+++ src/App.js
@@ -1,50 +1,61 @@
 import './App.css';
-import React, {useEffect, useState} from 'react';
-import AWS from 'aws-sdk';
+import React, { useEffect, useState } from 'react';
+import * as AWS from 'aws-sdk';
+import { AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY } from './APIkey';
 
-function App(){
+function App() {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [loginData, setLoginData] = useState(null);
 
-  useEffect(() => {
-    const accessDocClient = new AWS.DynamoDB.DocumentClient();
-
-    const params = {
-      TableName: 'forum-users',
-      KeyConditionExpression: '#un = :user',
-      ExpressionAttributeNames: {
-        '#un': 'username',
-      },
-      ExmpressionAttributeValues: {
-        ':user': username,
-      },
-      };
-
-      accessDocClient.query(params, (err,data) => {
-        if (err){
-          console.error('Error querying database', err);
-        } else{
-          setLoginData(data.Items.length > 0 ? data.ITems[0] : null);
-        }
-      });
-
-    }, [username]);
-   
-  AWS.config.update({
-    accessKeyId: AWS_ACCESS_KEY_ID,
-    secretAccessKey: AWS_SECRET_ACCESS_KEY,
-    region: 'us-east-2',
-  });
-
-  const handleLogin = (e) =>{
+  const handleLogin = (e) => {
     e.preventDefault();
-    if ((loginData) && (loginData.password === password)){
+
+    var AWS = require('aws-sdk');
+
+    AWS.config.update({
+      accessKeyId: AWS_ACCESS_KEY_ID,
+      secretAccessKey: AWS_SECRET_ACCESS_KEY,
+      region: 'us-east-2',
+    });
+
+    var ddb = new AWS.DynamoDB();
+
+    var params = {
+      TableName: 'forum-users',
+      KeyConditionExpression: 'username = :u',
+      ExpressionAttributeValues: {
+        ':u': {S: username},
+      },
+      ProjectionExpression: 'username, password, email',
+    };
+
+     ddb.query(params, (err, data) => {
+      if (err) {
+        console.error('Error querying database', err);
+      } else {
+        if (data.Items.length === 0){
+          console.log('No matching user found');
+        }
+        else{
+          const user = data.Items[0];
+          console.log(data.Items[0]);
+          if (user.password === password){
+            console.log('Matching user', data.Items[0]);
+            setLoginData(data.Items[0]);
+          }
+          else {
+            console.log("Incorrect password");
+          }
+        }
+      }
+    });
+
+    /*if (loginData && loginData.password === password) {
       console.log("logged in");
-    }
-    else{
-      console.log("login error")
-    }
+    } else {
+      console.log("login error");
+    }*/
   };
 
   return (
@@ -54,18 +65,18 @@ function App(){
       </header>
       <main className="App-main">
         <form className="App-form" onSubmit={handleLogin}>
-          <input 
-            className="App-form-input" 
+          <input
+            className="App-form-input"
             id="username"
             type="text"
             placeholder="Username"
             value={username}
             onChange={(u) => setUsername(u.target.value)}
           />
-          <input 
-            className="App-form-input" 
+          <input
+            className="App-form-input"
             id="password"
-            type="text"
+            type="password" // Password input type
             placeholder="Password"
             value={password}
             onChange={(p) => setPassword(p.target.value)}
@@ -74,8 +85,7 @@ function App(){
         </form>
       </main>
     </div>
-
   );
-};
+}
 
 export default App;
