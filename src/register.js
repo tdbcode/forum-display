3939e8a26174dcba85a81031b284adb1844afd90
Thomas Brady <thomasbrady87@gmail.com>
2023-10-26 19:21:50 -0500

    AWS Register Update 1

--- src/register.js
+++ src/register.js
@@ -6,35 +6,95 @@ function Register() {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
     const [registerError, setregisterError] = useState('');
+    const ddb = new AWS.DynamoDB();  // Connect to Dynamo Database via AWS
 
-    const handleRegister = ((h) => {
-        h.preventDefault(); //Prevent full refresh
-        
-        // Source: https://docs.aws.amazon.com/sdk-for-javascript/v2/developer-guide/dynamodb-examples-using-tables.html
-        // How to marshall: https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/DynamoDB/Converter.html#input-property 
-        var params = {
-            TableName: "forum-users",
-            Item: AWS.DynamoDB.Converter.marshall({
-                username : username,
-                email : email,
-                password: password
-            })
+    const ddbcheck = async (parameters, type, find) => {
+      var data;
+      try{
+        if (type === "query"){
+              data = await ddb.query(parameters).promise();
+          }
+          else if (type === "scan"){
+            data = await ddb.scan(parameters).promise();
+          }
+          // console.log(data); // for testing only
+          if (find){
+            return data.Items.length !== 0;
+          } 
+          else if(!find){
+            return data;
+          }
+        } catch(err){
+            console.error('Error querying database', err);
+        }
+    };
+
+    const handleRegister = async (e) => {
+        e.preventDefault(); //Prevent full refresh
+
+        //Check if requested username or email already exists
+        // Paramaters for username search, querying for the entered username and returning any records matching
+        const paramsUsername = {
+          TableName: 'forum-users',
+          KeyConditionExpression: 'username = :u',
+          ExpressionAttributeValues: {
+            ':u': {S: username},
+          },
+          ProjectionExpression: 'username'
         };
 
-        console.log(params);
+      var userExists = await ddbcheck(paramsUsername, "query", true);
+      
+      const paramsEmail = {
+        TableName: 'forum-users',
+        FilterExpression: 'email = :e',
+        ExpressionAttributeValues: {
+          ':e': { S: email },
+        },
+        ProjectionExpression: 'email',
+      };
 
-        var ddb = new AWS.DynamoDB();  // Connect to Dynamo Database via AWS
-        // Add items to database
-        ddb.putItem(params, function(err, data) {
-            if (err){
-                console.log(err, err.stack);
-                setregisterError("Database Registration Error.")
-            } 
-            else{
-                console.log(data);
-            }
-        });
-    });
+      var emailExists = await ddbcheck(paramsEmail, "scan", true);
+
+      // console.log('Username exists:', userExists); // for testing only
+      // console.log('Email exists:', emailExists); // for testing only
+      var canRegister;
+      if(!userExists && !emailExists){
+        canRegister = true;
+        setregisterError('');
+        registerUser();
+      } else if (userExists) {
+          setregisterError('Username already in use.');
+      }
+      else if (emailExists) {
+          setregisterError('Email already in use.');
+      }
+    };
+
+    const registerUser = () => {
+      // Source: https://docs.aws.amazon.com/sdk-for-javascript/v2/developer-guide/dynamodb-examples-using-tables.html
+      // How to marshall: https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/DynamoDB/Converter.html#input-property 
+      var params = {
+          TableName: "forum-users",
+          Item: AWS.DynamoDB.Converter.marshall({
+              username : username,
+              email : email,
+              password: password
+          })
+      };
+      console.log(params);
+                    
+      // Add items to database
+      ddb.putItem(params, function(err, data) {
+          if (err){
+              console.log(err, err.stack);
+              setregisterError("Database Registration Error.")
+          } 
+          else{
+              console.log(data);
+          }
+      });
+    };
 
     return(
         <div className="App">
@@ -58,7 +118,7 @@ function Register() {
             type="text"
             placeholder="Username"
             value={username}
-            onChange={(u) => setUsername(u.target.value)}
+            onChange={(e) => setUsername(e.target.value)}
           />
           </div>
           <div>
@@ -68,7 +128,7 @@ function Register() {
             type="password" // Password input type
             placeholder="Password"
             value={password}
-            onChange={(p) => setPassword(p.target.value)}
+            onChange={(e) => setPassword(e.target.value)}
           />
           </div>
           <input type="submit" className="App-form-button" value="Register"/>
@@ -80,4 +140,3 @@ function Register() {
 }
 
 export default Register;
-
